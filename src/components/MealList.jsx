91f8986ff54c9fa6aa6c0d2ac12458fa53791092
lsyokoo <lsy20041203@gmail.com>
2024-11-04 18:34:22 -0500

    fix base

--- src/components/MealList.jsx
+++ src/components/MealList.jsx
@@ -8,15 +8,17 @@
 import MealCard from './MealCard';
 
 const meals = [
-  { name: "Hummus", description: "A creamy blend of chickpeas, tahini, lemon, and garlic.", image: '/src/assets/mealImages/hummus.jpg' },
-  { name: "Bibimbap", description: "A Korean rice dish with assorted vegetables, meat, and a spicy sauce.", image: '/src/assets/mealImages/bibimbap.jpg' },
-  { name: "Vietnam Pho", description: "A Vietnamese soup consisting of broth, rice noodles, herbs, and meat.", image: '/src/assets/mealImages/pho.jpg' },
-  { name: "Caesar Salad", description: "A salad with romaine lettuce, croutons, and Caesar dressing.", image: '/src/assets/mealImages/caesar_salad.jpg' }
+  { name: "Hummus", description: "A creamy blend of chickpeas, tahini, lemon, and garlic.", image: '../assets/hummus.jpg' },
+  { name: "Bibimbap", description: "A Korean rice dish with assorted vegetables, meat, and a spicy sauce.", image: '../assets/bibimbap.jpg' },
+  { name: "Vietnam Pho", description: "A Vietnamese soup consisting of broth, rice noodles, herbs, and meat.", image: '../assets/pho.jpg' },
+  { name: "Caesar Salad", description: "A salad with romaine lettuce, croutons, and Caesar dressing.", image: '../assets/caesar_salad.jpg' }
 ];
 
 function MealList() {
   return (
     <div className="card-container">
+      hello
+      <img  src={"/assets/pho.jpg"}/>
       {meals.map((meal, index) => (
         <MealCard key={index} name={meal.name} description={meal.description} image={meal.image} />
       ))}
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'recipe-app-react'
+  base:'recipe_app_react'
 })
