8341e906db4fd4fa7d5127c12734efc43214d6f0
lsyokoo <lsy20041203@gmail.com>
2024-11-04 19:54:40 -0500

    photo update

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -4,18 +4,31 @@
 //     console.log(props.age)
 //   return <>
 //             <h1>{props.greeting}, {props.name}</h1>
-
 //         </>
 // }
 
 // export default Welcome;
-
 import './MealCard.css';
 
-function MealCard({ name, description, image }) {
+// Import images with the correct relative path
+import hummusImage from '../assets/hummus.jpg';
+import bibimbapImage from '../assets/bibimbap.jpg';
+import phoImage from '../assets/pho.jpg';
+import caesarSaladImage from '../assets/caesar_salad.jpg';
+
+function MealCard({ name, description }) {
+  // Map names to images
+  const images = {
+    Hummus: hummusImage,
+    Bibimbap: bibimbapImage,
+    Pho: phoImage,
+    "Caesar Salad": caesarSaladImage
+  };
+
   return (
     <div className="meal-card">
-      <img src={image} alt={name} />
+      {/* Use name to access the correct image */}
+      <img src={images[name]} alt={name} />
       <h3>{name}</h3>
       <p>{description}</p>
     </div>
